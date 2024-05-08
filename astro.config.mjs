aae8e98c2e684d23dc66b96b72f20e0a163eb784
ZephyroCode <royceskycraft@gmail.com>
2024-05-08 16:54:28 -0400

    Add GitHub pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://zephyrocode.github.io'
-});
+  site: 'https://zephyrocode.github.io',
+  base: 'students-app',
+})
